1268d2e27402e94f60423521c7183716dbd7417a
leonardocurtis <leonardo.bernardocurtis@hotmail.com>
2026-04-04 01:54:54 -0300

    feat(db): add initial database migration

---
+++ SalusMedApi/Infrastructure/Persistence/Migrations/20260404041041_InitialCreate.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SalusMedApi.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password_hash = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    role = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "patients",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    cpf = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    gender = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    date_of_birth = table.Column<DateOnly>(type: "date", nullable: false),
+                    status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    user_id = table.Column<long>(type: "bigint", nullable: false),
+                    city = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    complement = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    neighborhood = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    number = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    postal_code = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    state = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    street = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_patients", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_patients_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "physicians",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    medical_registration = table.Column<string>(type: "character varying(6)", maxLength: 6, nullable: false),
+                    cpf = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    gender = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    date_of_birth = table.Column<DateOnly>(type: "date", nullable: false),
+                    specialty = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    user_id = table.Column<long>(type: "bigint", nullable: false),
+                    city = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    complement = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    neighborhood = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    number = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    postal_code = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    state = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    street = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_physicians", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_physicians_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_patients_cpf",
+                table: "patients",
+                column: "cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_patients_phone",
+                table: "patients",
+                column: "phone",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_patients_user_id",
+                table: "patients",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_physicians_cpf",
+                table: "physicians",
+                column: "cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_physicians_medical_registration",
+                table: "physicians",
+                column: "medical_registration",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_physicians_phone",
+                table: "physicians",
+                column: "phone",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_physicians_user_id",
+                table: "physicians",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "patients");
+
+            migrationBuilder.DropTable(
+                name: "physicians");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
